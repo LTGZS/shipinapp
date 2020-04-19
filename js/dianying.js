7cf2fe85c417dfc0e563ecfb14bd055d8586b7a7
LIUTAO <48473805+LTGZS@users.noreply.github.com>
2020-04-19 22:02:58 +0800

    1

---
+++ js/dianying.js
@@ -0,0 +1,245 @@
+if(Hls.isSupported()) {
+					    var video = document.getElementById('zpz');
+					    var hls = new Hls();
+					    hls.loadSource('https://bilibili.com-l-163.com/20181022/14697_1eb39cc9/1000k/hls/index.m3u8');
+					    hls.attachMedia(video);
+					    hls.on(Hls.Events.MANIFEST_PARSED,function() {
+					      video.pause();
+					  });
+}
+if(Hls.isSupported()) {
+					    var video = document.getElementById('zpz2');
+					    var hls = new Hls();
+					    hls.loadSource('https://iqiyi.zuidameiju.com/20200304/13094_f52b1c2b/1000k/hls/index.m3u8');
+					    hls.attachMedia(video);
+					    hls.on(Hls.Events.MANIFEST_PARSED,function() {
+					      video.pause();
+					  });
+}
+if(Hls.isSupported()) {
+					    var video = document.getElementById('zpz3');
+					    var hls = new Hls();
+					    hls.loadSource('https://yi.jingdianzuida.com/20190905/RqA7ShXw/index.m3u8');
+					    hls.attachMedia(video);
+					    hls.on(Hls.Events.MANIFEST_PARSED,function() {
+					      video.pause();
+					  });
+}
+
+if(Hls.isSupported()) {
+					    var video = document.getElementById('zpz4');
+					    var hls = new Hls();
+					    hls.loadSource('https://bili.meijuzuida.com/20190929/21538_beabde01/1000k/hls/index.m3u8');
+					    hls.attachMedia(video);
+					    hls.on(Hls.Events.MANIFEST_PARSED,function() {
+					      video.pause();
+					  });
+}
+if(Hls.isSupported()) {
+					    var video = document.getElementById('zpz5');
+					    var hls = new Hls();
+					    hls.loadSource('https://yiyi.55zuiday.com/20180401/kMz3CRWq/index.m3u8');
+					    hls.attachMedia(video);
+					    hls.on(Hls.Events.MANIFEST_PARSED,function() {
+					      video.pause();
+					  });
+}
+if(Hls.isSupported()) {
+					    var video = document.getElementById('zpz6');
+					    var hls = new Hls();
+					    hls.loadSource('https://tudou.com-l-tudou.com/20181115/14291_9d55f4ad/1000k/hls/index.m3u8');
+					    hls.attachMedia(video);
+					    hls.on(Hls.Events.MANIFEST_PARSED,function() {
+					      video.pause();
+					  });
+}
+if(Hls.isSupported()) {
+					    var video = document.getElementById('zpz7');
+					    var hls = new Hls();
+					    hls.loadSource('https://wuji.zhulong-zuida.com/20191009/10506_d539f64b/1000k/hls/index.m3u8');
+					    hls.attachMedia(video);
+					    hls.on(Hls.Events.MANIFEST_PARSED,function() {
+					      video.pause();
+					  });
+}
+if(Hls.isSupported()) {
+					    var video = document.getElementById('zpz8');
+					    var hls = new Hls();
+					    hls.loadSource('https://leshi.cdn-zuyida.com/20170603/XgwO7tMy/600kb/hls/index.m3u8');
+					    hls.attachMedia(video);
+					    hls.on(Hls.Events.MANIFEST_PARSED,function() {
+					      video.pause();
+					  });
+}
+if(Hls.isSupported()) {
+					    var video = document.getElementById('zpz9');
+					    var hls = new Hls();
+					    hls.loadSource('https://xigua-cdn.haima-zuida.com/20200221/3241_d01ed457/1000k/hls/index.m3u8');
+					    hls.attachMedia(video);
+					    hls.on(Hls.Events.MANIFEST_PARSED,function() {
+					      video.pause();
+					  });
+}
+if(Hls.isSupported()) {
+					    var video = document.getElementById('zpz10');
+					    var hls = new Hls();
+					    hls.loadSource('https://yi.jingdianzuida.com/ppvod/439FFCF8115C129C7F0721A53404DA08.m3u8');
+					    hls.attachMedia(video);
+					    hls.on(Hls.Events.MANIFEST_PARSED,function() {
+					      video.pause();
+					  });
+}
+if(Hls.isSupported()) {
+					    var video = document.getElementById('zpz11');
+					    var hls = new Hls();
+					    hls.loadSource('https://wuji.zhulong-zuida.com/20190713/1749_91890377/800k/hls/index.m3u8');
+					    hls.attachMedia(video);
+					    hls.on(Hls.Events.MANIFEST_PARSED,function() {
+					      video.pause();
+					  });
+}
+if(Hls.isSupported()) {
+					    var video = document.getElementById('zpz12');
+					    var hls = new Hls();
+					    hls.loadSource('https://youku.cdn6-okzy.com/20200313/16323_49ad437e/1000k/hls/index.m3u8');
+					    hls.attachMedia(video);
+					    hls.on(Hls.Events.MANIFEST_PARSED,function() {
+					      video.pause();
+					  });
+}
+if(Hls.isSupported()) {
+					    var video = document.getElementById('zpz13');
+					    var hls = new Hls();
+					    hls.loadSource('https://yi.jingdianzuida.com/ppvod/C5347F94749018C3BCC6420D97559824.m3u8');
+					    hls.attachMedia(video);
+					    hls.on(Hls.Events.MANIFEST_PARSED,function() {
+					      video.pause();
+					  });
+}
+if(Hls.isSupported()) {
+					    var video = document.getElementById('zpz14');
+					    var hls = new Hls();
+					    hls.loadSource('https://iqiyi.zuidameiju.com/20200309/13354_072318d9/1000k/hls/index.m3u8');
+					    hls.attachMedia(video);
+					    hls.on(Hls.Events.MANIFEST_PARSED,function() {
+					      video.pause();
+					  });
+}
+if(Hls.isSupported()) {
+					    var video = document.getElementById('zpz15');
+					    var hls = new Hls();
+					    hls.loadSource('https://yiyi.55zuiday.com/ppvod/71E59D0410D65B241B8ACEABE5D77F6C.m3u8');
+					    hls.attachMedia(video);
+					    hls.on(Hls.Events.MANIFEST_PARSED,function() {
+					      video.pause();
+					  });
+}
+if(Hls.isSupported()) {
+					    var video = document.getElementById('zpz16');
+					    var hls = new Hls();
+					    hls.loadSource('https://youku.com-qq.net/20190626/9407_46a8d1a3/1000k/hls/index.m3u8');
+					    hls.attachMedia(video);
+					    hls.on(Hls.Events.MANIFEST_PARSED,function() {
+					      video.pause();
+					  });
+}
+if(Hls.isSupported()) {
+					    var video = document.getElementById('zpz17');
+					    var hls = new Hls();
+					    hls.loadSource('https://youku.com-youku.com/20180312/mvUL4b48/900kb/hls/index.m3u8');
+					    hls.attachMedia(video);
+					    hls.on(Hls.Events.MANIFEST_PARSED,function() {
+					      video.pause();
+					  });
+}
+if(Hls.isSupported()) {
+					    var video = document.getElementById('zpz18');
+					    var hls = new Hls();
+					    hls.loadSource('https://yi.jingdianzuida.com/20191117/6oZFKeQY/index.m3u8');
+					    hls.attachMedia(video);
+					    hls.on(Hls.Events.MANIFEST_PARSED,function() {
+					      video.pause();
+					  });
+}
+if(Hls.isSupported()) {
+					    var video = document.getElementById('zpz19');
+					    var hls = new Hls();
+					    hls.loadSource('https://iqiyi.zuidameiju.com/20200304/13094_f52b1c2b/1000k/hls/index.m3u8');
+					    hls.attachMedia(video);
+					    hls.on(Hls.Events.MANIFEST_PARSED,function() {
+					      video.pause();
+					  });
+}
+if(Hls.isSupported()) {
+					    var video = document.getElementById('zpz20');
+					    var hls = new Hls();
+					    hls.loadSource('https://iqiyi.zuidameiju.com/20200304/13094_f52b1c2b/1000k/hls/index.m3u8');
+					    hls.attachMedia(video);
+					    hls.on(Hls.Events.MANIFEST_PARSED,function() {
+					      video.pause();
+					  });
+}
+if(Hls.isSupported()) {
+					    var video = document.getElementById('zpz21');
+					    var hls = new Hls();
+					    hls.loadSource('https://iqiyi.zuidameiju.com/20200304/13094_f52b1c2b/1000k/hls/index.m3u8');
+					    hls.attachMedia(video);
+					    hls.on(Hls.Events.MANIFEST_PARSED,function() {
+					      video.pause();
+					  });
+}
+if(Hls.isSupported()) {
+					    var video = document.getElementById('zpz22');
+					    var hls = new Hls();
+					    hls.loadSource('https://iqiyi.zuidameiju.com/20200304/13094_f52b1c2b/1000k/hls/index.m3u8');
+					    hls.attachMedia(video);
+					    hls.on(Hls.Events.MANIFEST_PARSED,function() {
+					      video.pause();
+					  });
+}
+if(Hls.isSupported()) {
+					    var video = document.getElementById('zpz23');
+					    var hls = new Hls();
+					    hls.loadSource('https://iqiyi.zuidameiju.com/20200304/13094_f52b1c2b/1000k/hls/index.m3u8');
+					    hls.attachMedia(video);
+					    hls.on(Hls.Events.MANIFEST_PARSED,function() {
+					      video.pause();
+					  });
+}
+if(Hls.isSupported()) {
+					    var video = document.getElementById('zpz24');
+					    var hls = new Hls();
+					    hls.loadSource('https://iqiyi.zuidameiju.com/20200304/13094_f52b1c2b/1000k/hls/index.m3u8');
+					    hls.attachMedia(video);
+					    hls.on(Hls.Events.MANIFEST_PARSED,function() {
+					      video.pause();
+					  });
+}
+if(Hls.isSupported()) {
+					    var video = document.getElementById('zpz25');
+					    var hls = new Hls();
+					    hls.loadSource('https://iqiyi.zuidameiju.com/20200304/13094_f52b1c2b/1000k/hls/index.m3u8');
+					    hls.attachMedia(video);
+					    hls.on(Hls.Events.MANIFEST_PARSED,function() {
+					      video.pause();
+					  });
+}
+if(Hls.isSupported()) {
+					    var video = document.getElementById('zpz26');
+					    var hls = new Hls();
+					    hls.loadSource('https://iqiyi.zuidameiju.com/20200304/13094_f52b1c2b/1000k/hls/index.m3u8');
+					    hls.attachMedia(video);
+					    hls.on(Hls.Events.MANIFEST_PARSED,function() {
+					      video.pause();
+					  });
+}
+if(Hls.isSupported()) {
+					    var video = document.getElementById('zpz27');
+					    var hls = new Hls();
+					    hls.loadSource('https://iqiyi.zuidameiju.com/20200304/13094_f52b1c2b/1000k/hls/index.m3u8');
+					    hls.attachMedia(video);
+					    hls.on(Hls.Events.MANIFEST_PARSED,function() {
+					      video.pause();
+					  });
+}
+
