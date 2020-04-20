16955daa2f803e97bbde0572cead19a79f7ff07a
LIUTAO <48473805+LTGZS@users.noreply.github.com>
2020-04-20 14:49:52 +0800

    1

---
+++ js/spdb.js
@@ -0,0 +1,323 @@
+
+// 视频特推内容
+function myFunction() {
+		    var x = document.getElementById("spbfnr");
+		    if (x.style.display === "block") {
+		        x.style.display = "none";
+		    } else {
+		        x.style.display = "block";
+		    }
+			var video = document.getElementById('myVideo');
+			       if(video.paused){ //如果已暂停则播放
+			           video.pause(); //播放控制
+			       }else{ // 已播放点击则暂停
+			           video.pause(); //暂停控制
+			       }
+		}
+
+function myFunction2() {
+		    var x = document.getElementById("spbfnr2");
+		    if (x.style.display === "block") {
+		        x.style.display = "none";
+		    } else {
+		        x.style.display = "block";
+		    }
+			var video = document.getElementById('myVideo2');
+			       if(video.paused){ //如果已暂停则播放
+			           video.pause(); //播放控制
+			       }else{ // 已播放点击则暂停
+			           video.pause(); //暂停控制
+			       }
+		}
+function myFunction3() {
+		    var x = document.getElementById("spbfnr3");
+		    if (x.style.display === "block") {
+		        x.style.display = "none";
+		    } else {
+		        x.style.display = "block";
+		    }
+			var video = document.getElementById('myVideo3');
+			       if(video.paused){ //如果已暂停则播放
+			           video.pause(); //播放控制
+			       }else{ // 已播放点击则暂停
+			           video.pause(); //暂停控制
+			       }
+		}
+function myFunction4() {
+		    var x = document.getElementById("spbfnr4");
+		    if (x.style.display === "block") {
+		        x.style.display = "none";
+		    } else {
+		        x.style.display = "block";
+		    }
+			var video = document.getElementById('myVideo4');
+			       if(video.paused){ //如果已暂停则播放
+			           video.pause(); //播放控制
+			       }else{ // 已播放点击则暂停
+			           video.pause(); //暂停控制
+			       }
+		}	
+				
+function myFunction5() {
+		    var x = document.getElementById("spbfnr5");
+		    if (x.style.display === "block") {
+		        x.style.display = "none";
+		    } else {
+		        x.style.display = "block";
+		    }
+			var video = document.getElementById('myVideo5');
+			       if(video.paused){ //如果已暂停则播放
+			           video.pause(); //播放控制
+			       }else{ // 已播放点击则暂停
+			           video.pause(); //暂停控制
+			       }
+		}
+function myFunction6() {
+		    var x = document.getElementById("spbfnr6");
+		    if (x.style.display === "block") {
+		        x.style.display = "none";
+		    } else {
+		        x.style.display = "block";
+		    }
+			var video = document.getElementById('myVideo6');
+			       if(video.paused){ //如果已暂停则播放
+			           video.pause(); //播放控制
+			       }else{ // 已播放点击则暂停
+			           video.pause(); //暂停控制
+			       }
+		}
+function myFunction7() {
+		    var x = document.getElementById("spbfnr7");
+		    if (x.style.display === "block") {
+		        x.style.display = "none";
+		    } else {
+		        x.style.display = "block";
+		    }
+			var video = document.getElementById('myVideo7');
+			       if(video.paused){ //如果已暂停则播放
+			           video.pause(); //播放控制
+			       }else{ // 已播放点击则暂停
+			           video.pause(); //暂停控制
+			       }
+		}				
+function myFunction8() {
+		    var x = document.getElementById("spbfnr8");
+		    if (x.style.display === "block") {
+		        x.style.display = "none";
+		    } else {
+		        x.style.display = "block";
+		    }
+			var video = document.getElementById('myVideo8');
+			       if(video.paused){ //如果已暂停则播放
+			           video.pause(); //播放控制
+			       }else{ // 已播放点击则暂停
+			           video.pause(); //暂停控制
+			       }
+		}		
+function myFunction9() {
+		    var x = document.getElementById("spbfnr9");
+		    if (x.style.display === "block") {
+		        x.style.display = "none";
+		    } else {
+		        x.style.display = "block";
+		    }
+			var video = document.getElementById('myVideo9');
+			       if(video.paused){ //如果已暂停则播放
+			           video.pause(); //播放控制
+			       }else{ // 已播放点击则暂停
+			           video.pause(); //暂停控制
+			       }
+		}		
+function myFunction10() {
+		    var x = document.getElementById("spbfnr10");
+		    if (x.style.display === "block") {
+		        x.style.display = "none";
+		    } else {
+		        x.style.display = "block";
+		    }
+			var video = document.getElementById('myVideo10');
+			       if(video.paused){ //如果已暂停则播放
+			           video.pause(); //播放控制
+			       }else{ // 已播放点击则暂停
+			           video.pause(); //暂停控制
+			       }
+		}		
+function myFunction11() {
+		    var x = document.getElementById("spbfnr11");
+		    if (x.style.display === "block") {
+		        x.style.display = "none";
+		    } else {
+		        x.style.display = "block";
+		    }
+			var video = document.getElementById('myVideo11');
+			       if(video.paused){ //如果已暂停则播放
+			           video.pause(); //播放控制
+			       }else{ // 已播放点击则暂停
+			           video.pause(); //暂停控制
+			       }
+		}		
+function myFunction12() {
+		    var x = document.getElementById("spbfnr12");
+		    if (x.style.display === "block") {
+		        x.style.display = "none";
+		    } else {
+		        x.style.display = "block";
+		    }
+			var video = document.getElementById('myVideo12');
+			       if(video.paused){ //如果已暂停则播放
+			           video.pause(); //播放控制
+			       }else{ // 已播放点击则暂停
+			           video.pause(); //暂停控制
+			       }
+		}
+function myFunction13() {
+		    var x = document.getElementById("spbfnr13");
+		    if (x.style.display === "block") {
+		        x.style.display = "none";
+		    } else {
+		        x.style.display = "block";
+		    }
+			var video = document.getElementById('myVideo13');
+			       if(video.paused){ //如果已暂停则播放
+			           video.pause(); //播放控制
+			       }else{ // 已播放点击则暂停
+			           video.pause(); //暂停控制
+			       }
+		}				
+				
+function myFunction14() {
+		    var x = document.getElementById("spbfnr14");
+		    if (x.style.display === "block") {
+		        x.style.display = "none";
+		    } else {
+		        x.style.display = "block";
+		    }
+			var video = document.getElementById('myVideo14');
+			       if(video.paused){ //如果已暂停则播放
+			           video.pause(); //播放控制
+			       }else{ // 已播放点击则暂停
+			           video.pause(); //暂停控制
+			       }
+		}				
+function myFunction15() {
+		    var x = document.getElementById("spbfnr15");
+		    if (x.style.display === "block") {
+		        x.style.display = "none";
+		    } else {
+		        x.style.display = "block";
+		    }
+			var video = document.getElementById('myVideo15');
+			       if(video.paused){ //如果已暂停则播放
+			           video.pause(); //播放控制
+			       }else{ // 已播放点击则暂停
+			           video.pause(); //暂停控制
+			       }
+		}				
+function myFunction16() {
+		    var x = document.getElementById("spbfnr16");
+		    if (x.style.display === "block") {
+		        x.style.display = "none";
+		    } else {
+		        x.style.display = "block";
+		    }
+			// function toggleSound(){
+			      var video = document.getElementById('myVideo16');
+			       if(video.paused){ //如果已暂停则播放
+			           video.pause(); //播放控制
+			       }else{ // 已播放点击则暂停
+			           video.pause(); //暂停控制
+			       }
+			//     }
+			
+		}				
+	// function toggleSound(){
+	//       var video = document.getElementById('myVideo16');
+	 //       if(video.paused){ //如果已暂停则播放
+	 //           video.play(); //播放控制
+	 //       }else{ // 已播放点击则暂停
+	 //           video.pause(); //暂停控制
+	 //       }
+	//     }			
+				
+// 视频内容		
+function myFunction17() {
+		    var x = document.getElementById("spbfnr17");
+		    if (x.style.display === "block") {
+		        x.style.display = "none";
+		    } else {
+		        x.style.display = "block";
+		    }
+			var video = document.getElementById('myVideo17');
+			       if(video.paused){ //如果已暂停则播放
+			           video.pause(); //播放控制
+			       }else{ // 已播放点击则暂停
+			           video.pause(); //暂停控制
+			       }
+		}
+function myFunction18() {
+		    var x = document.getElementById("spbfnr18");
+		    if (x.style.display === "block") {
+		        x.style.display = "none";
+		    } else {
+		        x.style.display = "block";
+		    }
+			var video = document.getElementById('myVideo18');
+			       if(video.paused){ //如果已暂停则播放
+			           video.pause(); //播放控制
+			       }else{ // 已播放点击则暂停
+			           video.pause(); //暂停控制
+			       }
+		}						
+function myFunction19() {
+		    var x = document.getElementById("spbfnr19");
+		    if (x.style.display === "block") {
+		        x.style.display = "none";
+		    } else {
+		        x.style.display = "block";
+		    }
+			var video = document.getElementById('myVideo19');
+			       if(video.paused){ //如果已暂停则播放
+			           video.pause(); //播放控制
+			       }else{ // 已播放点击则暂停
+			           video.pause(); //暂停控制
+			       }
+		}	
+function myFunction20() {
+		    var x = document.getElementById("spbfnr20");
+		    if (x.style.display === "block") {
+		        x.style.display = "none";
+		    } else {
+		        x.style.display = "block";
+		    }
+			var video = document.getElementById('myVideo20');
+			       if(video.paused){ //如果已暂停则播放
+			           video.pause(); //播放控制
+			       }else{ // 已播放点击则暂停
+			           video.pause(); //暂停控制
+			       }
+		}	
+function myFunction21() {
+		    var x = document.getElementById("spbfnr21");
+		    if (x.style.display === "block") {
+		        x.style.display = "none";
+		    } else {
+		        x.style.display = "block";
+		    }
+			var video = document.getElementById('myVideo21');
+			       if(video.paused){ //如果已暂停则播放
+			           video.pause(); //播放控制
+			       }else{ // 已播放点击则暂停
+			           video.pause(); //暂停控制
+			       }
+		}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
---
+++ js/splj.js
@@ -0,0 +1,206 @@
+if(Hls.isSupported()) {
+		var video = document.getElementById('myVideo');
+		var hls = new Hls();
+		hls.loadSource('https://dbx4.tyswmp.com/20190406/Y1j2yoAw/index.m3u8');
+		hls.attachMedia(video);
+		hls.on(Hls.Events.MANIFEST_PARSED,function() {
+		  video.pause();
+		});
+	}	
+	
+if(Hls.isSupported()) {
+		var video = document.getElementById('myVideo2');
+		var hls = new Hls();
+		hls.loadSource('https://cdn-2.okp2pweb.com/hls/2020/03/01/vLU9XIo4/playlist.m3u8');
+		hls.attachMedia(video);
+		hls.on(Hls.Events.MANIFEST_PARSED,function() {
+		  video.pause();
+		});
+	}	
+
+if(Hls.isSupported()) {
+		var video = document.getElementById('myVideo3');
+		var hls = new Hls();
+		hls.loadSource('https://cdn-1.okp2pweb.com/hls/2019/10/29/V1hsLHCK/playlist.m3u8');
+		hls.attachMedia(video);
+		hls.on(Hls.Events.MANIFEST_PARSED,function() {
+		  video.pause();
+		});
+	}	
+			
+if(Hls.isSupported()) {
+		var video = document.getElementById('myVideo4');
+		var hls = new Hls();
+		hls.loadSource('https://youku.cdn7-okzy.com/20200328/18269_d20b7741/index.m3u8');
+		hls.attachMedia(video);
+		hls.on(Hls.Events.MANIFEST_PARSED,function() {
+		  video.pause();
+		});
+	}	
+	
+if(Hls.isSupported()) {
+		var video = document.getElementById('myVideo5');
+		var hls = new Hls();
+		hls.loadSource('https://cn2.18787000118.com/hls/20200325/8423fbcf98e916689f3a6d62f0b5c3fb/index.m3u8');
+		hls.attachMedia(video);
+		hls.on(Hls.Events.MANIFEST_PARSED,function() {
+		  video.pause();
+		});
+	}	
+	
+if(Hls.isSupported()) {
+		var video = document.getElementById('myVideo6');
+		var hls = new Hls();
+		hls.loadSource('https://youku.letv-cdn.com/2019/06/23/KE8e84LuASN9Zh29/playlist.m3u8');
+		hls.attachMedia(video);
+		hls.on(Hls.Events.MANIFEST_PARSED,function() {
+		  video.pause();
+		});
+	}	
+
+if(Hls.isSupported()) {
+		var video = document.getElementById('myVideo7');
+		var hls = new Hls();
+		hls.loadSource('https://haitang.kankan-kuyunzy.com/20200202/11025_085e371e/index.m3u8');
+		hls.attachMedia(video);
+		hls.on(Hls.Events.MANIFEST_PARSED,function() {
+		  video.pause();
+		});
+	}	
+			
+if(Hls.isSupported()) {
+		var video = document.getElementById('myVideo8');
+		var hls = new Hls();
+		hls.loadSource('https://vip.okokbo.com/20171220/ezRnUQML/index.m3u8');
+		hls.attachMedia(video);
+		hls.on(Hls.Events.MANIFEST_PARSED,function() {
+		  video.pause();
+		});
+	}				
+if(Hls.isSupported()) {
+		var video = document.getElementById('myVideo9');
+		var hls = new Hls();
+		hls.loadSource('https://cn4.ruioushang.com/hls/20190711/7fced146120ccf9ab0661be72d75d6ec/1562825376/index.m3u8');
+		hls.attachMedia(video);
+		hls.on(Hls.Events.MANIFEST_PARSED,function() {
+		  video.pause();
+		});
+	}	
+if(Hls.isSupported()) {
+		var video = document.getElementById('myVideo10');
+		var hls = new Hls();
+		hls.loadSource('https://s1.135-cdn.com/20191105/bNL2zs4Kukn031ge/index.m3u8');
+		hls.attachMedia(video);
+		hls.on(Hls.Events.MANIFEST_PARSED,function() {
+		  video.pause();
+		});
+	}		
+if(Hls.isSupported()) {
+		var video = document.getElementById('myVideo11');
+		var hls = new Hls();
+		hls.loadSource('https://bilibili.com-h-bilibili.com/20190328/8250_eff6acee/index.m3u8');
+		hls.attachMedia(video);
+		hls.on(Hls.Events.MANIFEST_PARSED,function() {
+		  video.pause();
+		});
+	}	
+if(Hls.isSupported()) {
+		var video = document.getElementById('myVideo12');
+		var hls = new Hls();
+		hls.loadSource('https://iqiyi.zuidameiju.com/20200309/13393_5e8d9dab/index.m3u8');
+		hls.attachMedia(video);
+		hls.on(Hls.Events.MANIFEST_PARSED,function() {
+		  video.pause();
+		});
+	}	
+if(Hls.isSupported()) {
+		var video = document.getElementById('myVideo13');
+		var hls = new Hls();
+		hls.loadSource('https://youku.com-l-youku.com/20181216/3982_d4991cf5/index.m3u8');
+		hls.attachMedia(video);
+		hls.on(Hls.Events.MANIFEST_PARSED,function() {
+		  video.pause();
+		});
+	}	
+if(Hls.isSupported()) {
+		var video = document.getElementById('myVideo14');
+		var hls = new Hls();
+		hls.loadSource('https://bili.let-1977cdn.com/20190622/LWHaAbLO/index.m3u8');
+		hls.attachMedia(video);
+		hls.on(Hls.Events.MANIFEST_PARSED,function() {
+		  video.pause();
+		});
+	}	
+if(Hls.isSupported()) {
+		var video = document.getElementById('myVideo15');
+		var hls = new Hls();
+		hls.loadSource('https://cn1.ruioushang.com/hls/20190206/1bb30a5c7c78a8ececfbf0c209a4c1b6/1549390439/index.m3u8');
+		hls.attachMedia(video);
+		hls.on(Hls.Events.MANIFEST_PARSED,function() {
+		  video.pause();
+		});
+	}	
+if(Hls.isSupported()) {
+		var video = document.getElementById('myVideo16');
+		var hls = new Hls();
+		hls.loadSource('https://sohu.com-v-sohu.com/20180830/12209_04e8a79a/index.m3u8');
+		hls.attachMedia(video);
+		hls.on(Hls.Events.MANIFEST_PARSED,function() {
+		  video.pause();
+		});
+	}		
+if(Hls.isSupported()) {
+		var video = document.getElementById('myVideo17');
+		var hls = new Hls();
+		hls.loadSource('https://xigua-cdn.haima-zuida.com/20200221/3241_d01ed457/1000k/hls/index.m3u8');
+		hls.attachMedia(video);
+		hls.on(Hls.Events.MANIFEST_PARSED,function() {
+		  video.pause();
+		});
+	}
+if(Hls.isSupported()) {
+		var video = document.getElementById('myVideo18');
+		var hls = new Hls();
+		hls.loadSource('https://wuji.zhulong-zuida.com/20190713/1749_91890377/800k/hls/index.m3u8');
+		hls.attachMedia(video);
+		hls.on(Hls.Events.MANIFEST_PARSED,function() {
+		  video.pause();
+		});
+	}
+if(Hls.isSupported()) {
+		var video = document.getElementById('myVideo19');
+		var hls = new Hls();
+		hls.loadSource('https://yi.jingdianzuida.com/ppvod/C5347F94749018C3BCC6420D97559824.m3u8');
+		hls.attachMedia(video);
+		hls.on(Hls.Events.MANIFEST_PARSED,function() {
+		  video.pause();
+		});
+	}	
+if(Hls.isSupported()) {
+		var video = document.getElementById('myVideo20');
+		var hls = new Hls();
+		hls.loadSource('https://youku.cdn6-okzy.com/20200313/16323_49ad437e/1000k/hls/index.m3u8');
+		hls.attachMedia(video);
+		hls.on(Hls.Events.MANIFEST_PARSED,function() {
+		  video.pause();
+		});
+	}						
+if(Hls.isSupported()) {
+		var video = document.getElementById('myVideo21');
+		var hls = new Hls();
+		hls.loadSource('https://yi.jingdianzuida.com/20190905/RqA7ShXw/index.m3u8');
+		hls.attachMedia(video);
+		hls.on(Hls.Events.MANIFEST_PARSED,function() {
+		  video.pause();
+		});
+	}						
+						
+						
+						
+						
+						
+						
+						
+						
+						
+						
